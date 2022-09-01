157fcd4c81868486c4eb809b829e809862cbbd0c
Nicolas Leszczynski <nicolaslesz@gmail.com>
2022-09-01 19:05:45 -0300

    entrega

--- MSC-SISTEMAS/front/src/pages/Contacto.js
+++ MSC-SISTEMAS/front/src/pages/Contacto.js
@@ -27,7 +27,7 @@ const Contacto = (props) => {
         setMsg('');
         setSending(true)
         const response = await
-        axios.post('http://localhost:3000/api/contacto', formData);
+        axios.post(`${process.env.REACT_APP_API_URL}/api/contacto`, formData);
         setSending(false);
         setMsg(response.data.message);
         if(response.data.error === false){
